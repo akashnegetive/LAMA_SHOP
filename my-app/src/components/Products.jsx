e67fc96ad8a2fb9a3269ca6e38b9bdc97963a3fb
akash <akashdba10@gmail.com>
2023-08-03 00:09:23 +0530

    update url

--- my-app/src/components/Products.jsx
+++ my-app/src/components/Products.jsx
@@ -30,8 +30,8 @@ const Container=styled.div`
      try{
           const res =await axios.get(
             cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://the-lama-shop.onrender.com/api/products?category=${cat}`
+            : "https://the-lama-shop.onrender.com/api/products"
           );
          setProducts(res.data);
      }catch(err){
--- my-app/src/requestMethod.js
+++ my-app/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://the-lama-shop.onrender.com/api/";
+const BASE_URL = "https://the-lama-shop.onrender.com";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
@@ -12,10 +12,10 @@ const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
   header: { token: `Bearer ${TOKEN}` },
 });
